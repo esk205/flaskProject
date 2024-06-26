754bcdbfcacd79ba96655c325767c73c03253704
Michael Wynnychuk <19212818+esk205@users.noreply.github.com>
2024-06-26 13:15:03 -0600

    Feat: Added an interactable web page

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+# Define a route for the root URL '/'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Get the name entered by the user
+        name = request.form['name']
+        # Render the template with the 'name' variable
+        return render_template('index.html', name=name)
+    # If it's a GET request or the form hasn't been submitted yet, render the template without 'name'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
